9541e443f9cad28e67ad16b3ecf4a7a1b1342ad6
mauricioAyllon <58120060+mauricioAyllon@users.noreply.github.com>
2020-05-14 10:26:58 -0400

    Added a better way to sum pandas columns

--- create_input_moon_FAN_F8.py
+++ create_input_moon_FAN_F8.py
@@ -14,33 +14,25 @@ import matplotlib.pyplot as plt
 import mcnpio as io
 
 # read in source probability distribution
-fileF4 = 'Moon/png_moon_V2.o'
+#fileF4 = 'Moon/png_moon_V2.o'
+fileF4 = 'Moon/test2-F4/png_moon_all_V3.o'
 df4 = io.read_output(fileF4, tally=4, n=1, tally_type='et', particle='p')
 
 df4.drop(columns=0.0, inplace=True)
 
-# sum over 5 time bins
-df4V2 = pd.DataFrame(columns=['energy'], data=df4.energy)
-tbins = 20
-for i in range(df4.shape[1]):
-    sum_cols = df4.iloc[:,tbins*i+1:tbins*i+(1+tbins)].sum(axis=1).to_numpy()
-    if sum_cols.sum() == 0:
-        next
-    else:
-        df_tmp = pd.DataFrame(columns=[i], data=sum_cols)
-        df4V2 = df4V2.join(df_tmp)
-
-prompt = df4V2.iloc[:,1:52].sum(axis=1)
-delay = df4V2.iloc[:,51:].sum(axis=1)
+df4V3 = df4.add_prefix('t')
+first_tentry = df4.columns[1]
+prompt = df4V3.loc[:,f't{first_tentry}':'t50.0'].sum(axis=1)
+delay = df4V3.loc[:,'t51.0':].sum(axis=1)
 
 prompt_prob = prompt/prompt.sum()
 delay_prob = delay/delay.sum()
 plt.figure();
-plt.step(df4V2.energy, prompt_prob, label='prompt: 0 - 50 us')
-plt.step(df4V2.energy, delay_prob, label='delay: 51 - 200 us')
+plt.step(df4V3.tenergy, prompt_prob, label='prompt: 0 - 50 us')
+plt.step(df4V3.tenergy, delay_prob, label='delay: 51 - 200 us')
 plt.legend()
 plt.title('Probability Distributions')
-plt.yscale('log')
+#plt.yscale('log')
 plt.xlabel('Energy (MeV)')
 
 # create input file
@@ -49,10 +41,10 @@ surfaces = ['test']
 materials = ['test']
 dataC = ['test']
 
-Ebin = np.array(df4V2.energy)
+Ebin = np.array(df4V3.tenergy)
 freq_delay = np.array(delay_prob)
 freq_prompt = np.array(prompt_prob)
 
-io.make_inp_DE(cells, surfaces, materials, dataC, 'F8-inp-delay.i', Ebin, freq_delay)
-io.make_inp_DE(cells, surfaces, materials, dataC, 'F8-inp-prompt.i', Ebin, freq_prompt)
+io.make_inp_DE(cells, surfaces, materials, dataC, 'F8-inp-delay2.i', Ebin, freq_delay)
+io.make_inp_DE(cells, surfaces, materials, dataC, 'F8-inp-prompt2.i', Ebin, freq_prompt)
 
