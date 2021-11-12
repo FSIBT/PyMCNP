5c1a950d384edec497286c932b6240825070ee2e
mauricioAyllon <58120060+mauricioAyllon@users.noreply.github.com>
2021-11-12 17:30:26 -0500

    Required packages molmass, fortranformat

--- setup.py
+++ setup.py
@@ -11,4 +11,8 @@ setup(
     packages=["mcnptools"],
     scripts=["MCNPtools-parse-ptrac"],
     package_data={"mcnptools": [os.path.join("mcnptools", "data", "*txt"),]},
+    install_requires=[
+        "fortranformat >= 1.0.1",
+        "molmass >= 2021.6.18",
+    ],
 )
