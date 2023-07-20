c717c37ea3207ec2fbe243258db256c89ae8a148
Arun Persaud <apersaud@lbl.gov>
2023-07-20 11:46:03 -0700

    MCNP input file parsing: add optional comments when writing to text file

--- src/mcnptools/input_reader/read.py
+++ src/mcnptools/input_reader/read.py
@@ -17,25 +17,50 @@ class Input:
         self.materials = [Material.from_mcnp(c) for c in self.lines["materials"]]
         self.data = [Data.from_mcnp(c) for c in self.lines["data"]]
 
-    def to_mcnp(self):
+    def to_mcnp(self, add_comments: bool = True) -> str:
         if self.message:
             out = self.message + "\n"
             out += self.title + "\n"
         else:
             out = self.title + "\n"
 
-        for x in [self.cells, None, self.surfaces, None, self.materials, self.data]:
-            if x is None:
-                out += "\n"
-            else:
-                for y in x:
-                    out += y.to_mcnp()
+        if add_comments:
+            out += self.add_header("cell definitions")
+        for item in self.cells:
+            out += item.to_mcnp()
+        out += "\n"
+
+        if add_comments:
+            out += self.add_header("surface definitions")
+        for item in self.surfaces:
+            out += item.to_mcnp()
+        out += "\n"
+
+        if add_comments:
+            out += self.add_header("material compositions")
+        for item in self.materials:
+            out += item.to_mcnp()
+            if add_comments:
+                out += "C \n"
+
+        if add_comments:
+            out += self.add_header("source definition")
+        for item in self.data:
+            out += item.to_mcnp()
+        out += "\n"
 
         # wrap the text
         out = out.split("\n")
         out = ["\n".join(wrap(x, width=80, subsequent_indent="     ")) for x in out]
         out = "\n".join(out) + "\n"
 
+        return out
+
+    def add_header(self, name: str) -> str:
+        out = ""
+        out += "C " + "=" * 60 + "\n"
+        out += "C " + " " * 22 + f"{name}" + "\n"
+        out += "C " + "=" * 60 + "\n"
         return out
 
     @property
