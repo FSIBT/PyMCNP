acb17c1d3a226898d23ab4205dc28e7df2369b98
Arun Persaud <apersaud@lbl.gov>
2023-07-31 10:51:39 -0700

    MCNPtools-format: add more output options (side-by-side/diff/new only)

--- src/mcnptools/cli/format.py
+++ src/mcnptools/cli/format.py
@@ -1,5 +1,10 @@
 """Usage:
-   mcnptools-format <inputfile>
+   mcnptools-format (-s|-u|-n) <inputfile>
+
+Options:
+  -s --side-by-side    plot old and new side by side
+  -u --unified         unified diff
+  -n                   new file only
 
 Plots the input file and parsed and reformatted version side-by-side.
 Works best on a wide screen. This program is at the moment mostly used
@@ -8,6 +13,7 @@ to veryfiy and test the parsing code.
 """
 
 
+import difflib
 from pathlib import Path
 from itertools import zip_longest
 import sys
@@ -20,7 +26,7 @@ import mcnptools as mt
 
 def main():
     command = docopt.docopt(__doc__)
-    # print(command)
+    print(command)
 
     INPUT = Path(command["<inputfile>"])
 
@@ -32,12 +38,24 @@ def main():
 
     new = mt.input_reader.Input(INPUT).to_mcnp()
 
-    for a, b in zip_longest(orig.split("\n"), new.split("\n")):
-        if a is None:
-            a = ""
-        if b is None:
-            b = ""
-        print(f"{a.rstrip():<90} | {b.rstrip():<90}")
+    if command["--side-by-side"]:
+        for a, b in zip_longest(orig.split("\n"), new.split("\n")):
+            if a is None:
+                a = ""
+            if b is None:
+                b = ""
+            print(f"{a.rstrip():<90} | {b.rstrip():<90}")
+    elif command["--unified"]:
+        a, b = orig.split("\n"), new.split("\n")
+        for line in difflib.unified_diff(a, b, fromfile="original", tofile="formatted"):
+            if line[0] == "+":
+                print(f"[green]{line[0]}[/]{line[1:]}")
+            elif line[0] == "-":
+                print(f"[red]{line[0]}[/]{line[1:]}")
+            else:
+                print(line)
+    else:
+        print(new)
 
 
 if __name__ == "__main__":
