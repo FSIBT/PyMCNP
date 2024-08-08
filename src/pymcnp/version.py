a919fe5bc627a89a0cdcf09e0efa44532131dfb1
Devin Pease <bitterestb0ng0@gmail.com>
2024-08-08 16:23:19 -0700

    Updated .gitignore

--- src/pymcnp/version.py
+++ src/pymcnp/version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1+g9fbf517.d20240802'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g9fbf517.d20240802')
+__version__ = version = '0.0.0'
+__version_tuple__ = version_tuple = (0, 0, 0)
