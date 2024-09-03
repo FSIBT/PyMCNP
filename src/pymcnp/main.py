37cd10dd09c6f05ad468feabb7a67278b85d24f8
BitterB0NG0 <93603537+BitterB0NG0@users.noreply.github.com>
2024-09-03 09:01:21 -0700

    Fixed main.py Script Execution

--- src/pymcnp/main.py
+++ src/pymcnp/main.py
@@ -9,11 +9,8 @@ Functions:
 import os
 import sys
 
-from . import version
-from .cli import run
-from .cli import ls
-from .cli import inpt
-from .cli import _io
+import pymcnp.cli
+import pymcnp.version as version
 
 
 PYMCNP_DIR = ".pymcnp/"
@@ -39,17 +36,17 @@ def main(argv: list[str] = sys.argv[1:]) -> None:
     # Processing Command
     match argv[0] if argv else None:
         case "ls":
-            ls.main(argv[1:])
+            pymcnp.cli.ls.main(argv[1:])
         case "run":
-            run.main(argv[1:])
+            pymcnp.cli.run.main(argv[1:])
         case "input":
-            inpt.main(argv[1:])
+            pymcnp.cli.inpt.main(argv[1:])
         case "help":
             print("HELP :)")
         case None:
             print(PYMCNP_TITLE)
         case _:
-            _io.error(_io.ERROR_UNRECOGNIZED_ARGS)
+            pymcnp.cli._io.error(_io.ERROR_UNRECOGNIZED_ARGS)
 
 
 if __name__ == "__main__":
