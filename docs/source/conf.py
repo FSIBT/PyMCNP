96635ee9b3eddd2d1550cbe17847d92c19273914
Arun Persaud <apersaud@lbl.gov>
2024-09-29 11:26:04 -0700

    Docs: fix issues with readthe docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,5 +23,5 @@ autodoc_member_order = "bysource"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
-html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+html_static_path = []
