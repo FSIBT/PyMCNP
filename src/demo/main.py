dc42efe38a1515fad42dbf27999eab606926d7f1
BitterB0NG0 <93603537+BitterB0NG0@users.noreply.github.com>
2024-10-16 01:14:06 -0700

    Added demo Command

---
+++ src/demo/main.py
@@ -0,0 +1,24 @@
+import sys
+
+import docopt
+
+
+DEMO_DOCOPT = """
+Usage:
+    demo <inp>
+"""
+
+
+def main():
+    args = docopt.docopt(DEMO_DOCOPT)
+
+    try:
+        open(args["<inp>"])
+        print(f"[DEMO] Args: {' '.join(sys.argv)}")
+    except FileNotFoundError:
+        print("[DEMO] Input file not found.")
+        exit(1)
+
+    file = open("." + "".join(args["<inp>"].split(".")[:-1]) + ".outp", "x")
+    file.write("MCNP Output! :)")
+    file.close()
