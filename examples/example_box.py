507f4c0b9291f1eadfb829806f2899f613b4c922
BitterB0NG0 <93603537+BitterB0NG0@users.noreply.github.com>
2025-02-17 16:01:11 -0700

    Updated ``example_box.py``

--- examples/example_box.py
+++ examples/example_box.py
@@ -1,26 +1,22 @@
 import pymcnp
 
 
-RADIUS_AIR = 60
-RADIUS_SHIELD = 15
-RADIUS_LEAD = 1
+def get_inp(RADIUS_AIR, RADIUS_SHIELD, RADIUS_LEAD):
+    BOX_AIR = pymcnp.inp.SurfaceOption_Rpp.from_mcnp(
+        f'rpp {-RADIUS_AIR} {RADIUS_AIR} {-RADIUS_AIR} {RADIUS_AIR} {-RADIUS_AIR} {RADIUS_AIR}'
+    )
+    BOX_SHIELD = pymcnp.inp.SurfaceOption_Rpp.from_mcnp(
+        f'rpp {-RADIUS_SHIELD} {RADIUS_SHIELD} {-RADIUS_SHIELD} {RADIUS_SHIELD} {-RADIUS_SHIELD} {RADIUS_SHIELD}'
+    )
+    BOX_LEAD = pymcnp.inp.SurfaceOption_Rpp.from_mcnp(
+        f'rpp {-RADIUS_LEAD} {RADIUS_LEAD} {-RADIUS_LEAD} {RADIUS_LEAD} {-RADIUS_LEAD} {RADIUS_LEAD}'
+    )
 
-BOX_AIR = pymcnp.inp.SurfaceOption_Rpp.from_mcnp(
-    f'rpp {-RADIUS_AIR} {RADIUS_AIR} {-RADIUS_AIR} {RADIUS_AIR} {-RADIUS_AIR} {RADIUS_AIR}'
-)
-BOX_SHIELD = pymcnp.inp.SurfaceOption_Rpp.from_mcnp(
-    f'rpp {-RADIUS_SHIELD} {RADIUS_SHIELD} {-RADIUS_SHIELD} {RADIUS_SHIELD} {-RADIUS_SHIELD} {RADIUS_SHIELD}'
-)
-BOX_LEAD = pymcnp.inp.SurfaceOption_Rpp.from_mcnp(
-    f'rpp {-RADIUS_LEAD} {RADIUS_LEAD} {-RADIUS_LEAD} {RADIUS_LEAD} {-RADIUS_LEAD} {RADIUS_LEAD}'
-)
+    MATERIAL_AIR = pymcnp.inp.DataOption_M.from_formula(1, {'N2': 0.8, 'O2': 0.2})
+    MATERIAL_SHIELD = pymcnp.inp.DataOption_M.from_formula(2, {'TiO2': 0.5, 'PbO': 0.5})
+    MATERIAL_LEAD = pymcnp.inp.DataOption_M.from_formula(3, {'Pb': 1})
 
-MATERIAL_AIR = pymcnp.inp.DataOption_M.from_formula(1, {'N2': 0.8, 'O2': 0.2})
-MATERIAL_SHIELD = pymcnp.inp.DataOption_M.from_formula(2, {'TiO2': 0.5, 'PbO': 0.5})
-MATERIAL_LEAD = pymcnp.inp.DataOption_M.from_formula(3, {'Pb': 1})
-
-inp = pymcnp.inp.Inp.from_mcnp(
-    f"""
+    return f"""
 Box Example
 10 1 0.5 20
 11 2 0.5 21:20
@@ -32,8 +28,11 @@ Box Example
 22 {BOX_LEAD}
 23 SO {RADIUS_AIR + RADIUS_SHIELD + RADIUS_LEAD + 1}
 
+{MATERIAL_AIR}
+{MATERIAL_SHIELD}
+{MATERIAL_LEAD}
 SDEF X=0 Y=0 Z=0 ERG=2.2
 """[1:-1]
-)
 
-print(inp.data)
+
+print(get_inp(60, 15, 1).to_mcnp())
