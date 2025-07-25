8a9ca417d9a917d313acc5a8c25c75c53f42e227
BitterB0NG0 <93603537+BitterB0NG0@users.noreply.github.com>
2025-07-24 18:15:29 -0700

    tests: updated ``subprocess.run`` to ``os.system``

--- tests/test_pymcnp/test__cli/test_main.py
+++ tests/test_pymcnp/test__cli/test_main.py
@@ -1,13 +1,13 @@
-import subprocess
+import os
 
 
 class Test_Main:
     class Test_Main:
         def test(self):
-            subprocess.run(['pymcnp', 'help', 'check'])
-            subprocess.run(['pymcnp', 'help', 'convert'])
-            subprocess.run(['pymcnp', 'help', 'plot'])
-            subprocess.run(['pymcnp', 'help', 'run'])
-            subprocess.run(['pymcnp', 'help', 'visualize'])
-            subprocess.run(['pymcnp', 'help'])
-            subprocess.run(['pymcnp', 'help', 'hello'])
+            os.system('pymcnp help')
+            os.system('pymcnp help check')
+            os.system('pymcnp help convert')
+            os.system('pymcnp help plot')
+            os.system('pymcnp help run')
+            os.system('pymcnp help visualize')
+            os.system('pymcnp help hello')
