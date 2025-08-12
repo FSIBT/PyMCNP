9799accff23daf7c04b0f81be1ec127eb5724468
BitterB0NG0 <93603537+BitterB0NG0@users.noreply.github.com>
2025-08-12 14:13:13 -0700

    inp: added cutoff to `from_formula`

--- examples/create_material.py
+++ examples/create_material.py
@@ -6,7 +6,7 @@ import pymcnp
 
 # Creating material using ``__init__``.
 material = pymcnp.inp.data.M_0(
-    suffix=1, substances=['001001 0.1118855432927602', '001002 1.2868317335160966e-05', '008016 0.8859435015301171', '008017 0.00033747860358816377', '008018 0.0018206082561993046']
+    suffix=1, substances=['001001 0.1118855432927602', '008016 0.8859435015301171']
 )
 
 print(material)
